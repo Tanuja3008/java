d7a55200cb3db2eafe561ce72b6d5bd2958f0145
Tanuja Thombare <tanujathombare3008@gmail.com>
2026-07-15 23:38:29 +0530

    Add files via upload

---
+++ Converting.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Converting{
+
+	public static void main(String [] arg){
+		String s = "LLLVVP";
+			char [] c = s.toCharArray();
+			int count = 1;
+			for(int i = 0; i < c.length();i++){
+	 			if(int i =0; i < )
+					if(c[i]==c[j]){
+						count++;
+			
+		System.out.println()
+	}
+}
+	}
+
+}
